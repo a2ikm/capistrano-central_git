b711f1e5d3e19e8ce89092ecf068b00b7f3ea0d4
Masato Ikeda <masato.ikeda@gmail.com>
2015-07-09 01:44:56 +0900

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source 'https://rubygems.org'
+
+# Specify your gem's dependencies in capistrano-central_git.gemspec
+gemspec
---
+++ lib/capistrano/central_git.rb
@@ -0,0 +1,7 @@
+require "capistrano/central_git/version"
+
+module Capistrano
+  module CentralGit
+    # Your code goes here...
+  end
+end
